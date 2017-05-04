6e44362a36104c9d7a8963671ce847bc7dd17b0e
Dryhad <williamwhatsit@yahoo.com>
2017-05-04 11:18:36 +1000

    Uploading everything so far

---
+++ After the End - EU4 Converter/eu4_converter/copy/common/defines/00_late.lua
@@ -0,0 +1,5 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+NDefines.NGame.START_DATE = "3053.1.1"
+NDefines.NGame.END_DATE = "3421.1.2"
---
+++ AfterTheEnd_converter.mod
@@ -0,0 +1,4 @@
+name = "After the End - EU4 Converter"
+path = "mod/After the End - EU4 Converter"
+
+replace_path = "eu4_converter/sunset_invasion/"
